8ab31e0a9881b10d4ffe5b003c60356dfb09aa1e
Anthony Scopatz <scopatz@gmail.com>
2014-01-23 03:00:38 +0000

    ini rc files

---
+++ dagmcrc.py
@@ -0,0 +1,18 @@
+server_name = server_url = 'dagmc.fuelcycle.org'
+port = 80
+
+# GitHub setting
+github_owner = 'svalinn'
+github_repo = 'DAGMC'
+github_user = 'dagmcci'
+github_credentials = '/root/svalinn-polyphemer/gh.cred'
+
+# BaTLab setting
+batlab_user = 'dagmcci'
+batlab_scripts_url = 'https://github.com/svalinn/dagmc-ci/archive/master.zip'
+batlab_fetch_file = 'fetch/dagmc.git'
+batlab_run_spec = 'fludag.run-spec'
+batlab_jobs_cache = '/var/www/dagmc.fuelcycle.org/jobs.cache'
+
+# Apache settings
+log_dir = '/root/svalinn-polyphemer'
